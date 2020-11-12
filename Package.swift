bf794a73d180f401df66c1671ecd77edcdb65eaf
Anton Chuev <anton.chuev@applitools.com>
2020-11-12 16:27:11 +0200

    Update manifest for EyesImages

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "EyesXCUI",
+    name: "EyesImages",
     platforms: [
             .iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "EyesXCUI",
-            targets: ["EyesXCUI"]),
+            name: "EyesImages",
+            targets: ["EyesImages"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "EyesXCUI",
-                              url: "https://applitools.bintray.com/iOS/EyesXCUIPackage/8.8.1/EyesXCUI.zip",
-                              checksum: "8efdb0040ebbc7885cd6e0bc108ed45e410e33a8d762d87f2e0cc984154ed50a"),
+        .binaryTarget(name: "EyesImages",
+                              url: "https://applitools.bintray.com/iOS/EyesImagesSwiftPackage/4.9.0/EyesImages.zip",
+                              checksum: "c3e8e5775216afb8b455f7e22d16c454e76987f56e4263e0c21a60299390cb17"),
     ]
 )
